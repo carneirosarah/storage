0f2db7a1c180b1b470d104219b7451dfb9afd4b6
carneirosarah <sarahrlcarneiro@gmail.com>
2021-01-20 21:13:00 -0300

    primeiro commit

---
+++ app/src/main/java/com/example/firestorage/MainActivity.kt
@@ -0,0 +1,75 @@
+package com.example.firestorage
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.app.AlertDialog
+import android.content.Intent
+import android.util.Log
+import android.widget.Toast
+import com.google.firebase.storage.FirebaseStorage
+import com.google.firebase.storage.StorageReference
+import com.squareup.picasso.Picasso
+import dmax.dialog.SpotsDialog
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    lateinit var alertDialog: AlertDialog
+    lateinit var storageReference: StorageReference
+    private val CODE_IMG = 1000
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        config()
+
+        fb_upload.setOnClickListener {
+            setIntent()
+        }
+    }
+
+    fun config() {
+
+        alertDialog = SpotsDialog.Builder().setContext(this).build()
+        storageReference = FirebaseStorage.getInstance().getReference("prod_img")
+    }
+
+    // Configura a intent para obter a imagem da galeria
+    fun setIntent() {
+
+        val intent = Intent()
+        intent.type = "image/"
+        intent.action = Intent.ACTION_GET_CONTENT
+        startActivityForResult(Intent.createChooser(intent, "Captura Imagem"), CODE_IMG)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if(requestCode == CODE_IMG) {
+
+            val uploadTask = storageReference.putFile(data!!.data!!)
+            val task = uploadTask.continueWithTask { task ->
+
+                if (task.isSuccessful) {
+
+                    Toast.makeText(this, "Chegando", Toast.LENGTH_SHORT).show()
+                }
+                storageReference!!.downloadUrl
+            }.addOnCompleteListener { task ->
+
+                if (task.isSuccessful) {
+
+                    val downloadUri = task.result
+                    val url = downloadUri!!.toString()
+                        .substring(0, downloadUri.toString().indexOf("&token"))
+                    Log.i("Link Direto ", url)
+                    alertDialog.dismiss()
+                    Picasso.get().load(url).into(iv_rec)
+                }
+            }
+        }
+    }
+}
